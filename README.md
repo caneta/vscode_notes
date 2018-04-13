2a788a0aeba1d0d88d22d07dc9b37d061f43eabf
alessandro.candini <candini@ariadne.it>
2018-04-13 08:48:27 +0200

    Add first draft of vscode conf file

---
+++ README.md
@@ -0,0 +1,3 @@
+# VSCode Notes
+
+Notes, confs, tip and tricks for the [Visual Studio Code](https://code.visualstudio.com/) editor by Microsoft
---
+++ conf/vscode.js
@@ -0,0 +1,10 @@
+{
+  "vim.disableAnnoyingNeovimMessage": true,
+  "git.ignoreLegacyWarning": true,
+  "workbench.colorTheme": "Cobalt2",
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.tabSize": 2,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  }
+}
